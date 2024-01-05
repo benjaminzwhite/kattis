16445b05ce13b5686d1f7b0ad48295a8782464eb
benjaminzwhite <benjaminzwhite@gmail.com>
2024-01-05 20:13:38 +0100

    Fixed indentation on some exercises

--- src/owlandfox.py
+++ src/owlandfox.py
@@ -9,7 +9,7 @@ for _ in range(T):
     s = input()
     for i, c in enumerate(s[::-1]):
         if c != '0':
-        	# i!=0 condition is to avoid cases where rightmost char is nonzero e.g. for input s=199 gives 198199 since s[-0:] = 199
+            # i!=0 condition is to avoid cases where rightmost char is nonzero e.g. for input s=199 gives 198199 since s[-0:] = 199
             res = s[:-i-1] + str(int(c)-1) + s[-i:] * (i != 0)
             print(int(res))
             break
