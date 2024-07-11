c0c1993ff64bebd97eb41eb705e60763d900fc36
benjaminzwhite <benjaminzwhite@gmail.com>
2024-07-12 00:31:59 +0200

    Added 1 solved exercise

---
+++ src/isithalloween.py
@@ -0,0 +1,11 @@
+# IsItHalloween.com
+# https://open.kattis.com/problems/isithalloween
+# TAGS: basic
+# CP4: 1.4c, Selection Only
+# NOTES:
+s = input()
+
+if s in {"OCT 31", "DEC 25"}:
+    print("yup")
+else:
+    print("nope")
