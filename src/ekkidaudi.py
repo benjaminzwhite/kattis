10754a26157a76d79e1611c908db64e8b89dbb82
benjaminzwhite <benjaminzwhite@gmail.com>
2025-10-06 22:06:02 +0200

    Added 2 solved exercises

---
+++ src/ekkidaudi.py
@@ -0,0 +1,9 @@
+# Ekki dauði opna inni
+# https://open.kattis.com/problems/ekkidaudi
+# TAGS: basic
+# CP4: 1.4b, I/O + Sequences Only
+# NOTES:
+a, b = input().split('|')
+c, d = input().split('|')
+
+print(a + c, b + d)
---
+++ src/vidsnuningur.py
@@ -0,0 +1,10 @@
+# Viðsnúningur
+# https://open.kattis.com/problems/vidsnuningur
+# TAGS: basic
+# CP4: 1.4a, One-Liner I/O
+# NOTES:
+s = input()
+
+res = s[::-1]
+
+print(res)
