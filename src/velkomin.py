9e45647e84a89f9caadc35e542b8aba251219db4
benjaminzwhite <benjaminzwhite@gmail.com>
2025-10-09 19:00:16 +0200

    Added 1 solved exercise

---
+++ src/velkomin.py
@@ -0,0 +1,9 @@
+# Velkomin!
+# https://open.kattis.com/problems/velkomin
+# TAGS: basic
+# CP4: 1.4a, One-Liner I/O
+# NOTES:
+"""
+Not the most difficult Kattis exercise, but nice to learn about Iceland
+"""
+print("VELKOMIN!")
