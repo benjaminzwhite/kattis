04a793e6ded9aeda293cd94f073f6d410dc28e18
benjaminzwhite <benjaminzwhite@gmail.com>
2025-10-24 21:34:44 +0200

    Added 1 solved exercise

---
+++ src/stafur.py
@@ -0,0 +1,13 @@
+# Stafur
+# https://open.kattis.com/problems/stafur
+# TAGS: basic
+# CP4: 1.4d, Selection Only, 3+ Cases
+# NOTES:
+c = input()
+
+if c in "AEIOU":
+    print("Jebb")
+elif c == "Y":
+    print("Kannski")
+else:
+    print("Neibb")
